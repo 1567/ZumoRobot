561c4a8e76e12e73fe001eed4399ab31804581ab
Babayka108 <sevalander@gmail.com>
2014-01-10 17:53:48 +0300

    BorderControl_0.2.cpp
    Кажется, лишь я чем-то занялся на каникулах...

--- BorderControl_0.1.cpp
+++ BorderControl_0.1.cpp
@@ -1,36 +1,35 @@
 //Комменты будут русскими
-int ColourLineSensor[2][2];    //4 наших сенсора. Первая координата - передний, задний. Вторая - левый, правый.
-ColourLineSensor[0][0] = 8;
-ColourLineSensor[0][1] = 9;
-ColourLineSensor[1][0] = 10;
-ColourLineSensor[1][1] = 11;
-int PowerLineSensor=12;
+
+int ColourLineSensor[] = {8,9,10,11};    //ПЛ (Передний правый), ПП, ЗЛ, ЗП
+int PowerLineSensors=12;
 int DirectionOfEngine[] = {7,4};    //левый - 0, правый - 1
-int SpeedOfEngine[]={6,5};
+int SpeedOfEngine[] = {6,5};
 int MaxVelocity = 153;        //не уверен, что верно помню. Поправь, если что.
 int VOfSlow = 0;               //скорость более медленного колеса
-boolean Slow=0;                //какой из них медленнее
+int Slow = 0;                //какой из них медленнее
+int flag = 0;
 
 
 void setup()
 {
-  pinMode(ColourLineSensor[0][0],INPUT);
-  pinMode(ColourLineSensor[0][1],INPUT);
-  pinMode(ColourLineSensor[1][0],INPUT);
-  pinMode(ColourLineSensor[1][1],INPUT);
-  pinMode(PowerLineSensor,OUTPUT);
+  pinMode(ColourLineSensor[0],INPUT);
+  pinMode(ColourLineSensor[1],INPUT);
+  pinMode(ColourLineSensor[2],INPUT);
+  pinMode(ColourLineSensor[3],INPUT);
+  pinMode(PowerLineSensors,OUTPUT);
   pinMode(DirectionOfEngine[0],OUTPUT);
   pinMode(SpeedOfEngine[0],OUTPUT);
   pinMode(SpeedOfEngine[1],OUTPUT);
   pinMode(DirectionOfEngine[1],OUTPUT);
   
-  digitalWrite(PowerLineSensor,HIGH);
+  digitalWrite(PowerLineSensors,HIGH);
   
   digitalWrite(DirectionOfEngine[1],HIGH);
   digitalWrite(DirectionOfEngine[0],HIGH);
-  VOfSlow = MaxVelocity;
   analogWrite(SpeedOfEngine[0],MaxVelocity);    
   analogWrite(SpeedOfEngine[1],MaxVelocity);
+  
+  VOfSlow = MaxVelocity;
 }
 void loop()
 {
@@ -40,24 +39,29 @@ void loop()
   при этом колёса той стороны, с которой считали белый сигнал
   крутятся активнее для обеспечения нужного поворота.
   */
-  //We don't need to know, where the center is, you mean?  //Да, именно так. Но поворот устроен так, что едем мы к центру.
-  for(boolean i=0;i<2;i++){            // перед/зад
-    for(boolean j=0;j<2;j++){          // лево/право
-      if (digitalRead(ColourLineSensor[i][j])== HIGH){
-        digitalWrite(DirectionOfEngine[0],i);          //тут я считаю, что движение "вперёд" (так, что передние датчики - голова) - это LOW
-        digitalWrite(DirectionOfEngine[1],i);   //Is "HIGH" like "TRUE" like "1"? That is the question.   //При digitalWrite - да. При analogWrite - нет.
-        VOfSlow = MaxVelocity/2;
-        Slow = !j;                                      //какой из двигателей медленнее;
-        analogWrite(SpeedOfEngine[j],MaxVelocity);      //колёса со стороны сигналящего датчика активнее
-        analogWrite(SpeedOfEngine[!j],VOfSlow);
-        flag = 1;            //нужно ли ускорять второй мотор
+  for(int i=0;i<4;i++){            
+    if (digitalRead(ColourLineSensor[i])== HIGH){
+      
+      if (i == 0 or i == 1){
+        digitalWrite(DirectionOfEngine[0],1);          //тут я считаю, что движение "вперёд" (так, что передние датчики - голова) - это LOW
+        digitalWrite(DirectionOfEngine[1],1);
+      } else {
+        digitalWrite(DirectionOfEngine[0],0);
+        digitalWrite(DirectionOfEngine[1],0);
       }
+      
+      VOfSlow = MaxVelocity/3;
+      
+      Slow = (i+1)%2;                                      //какой из двигателей медленнее;
+      analogWrite(SpeedOfEngine[i%2],MaxVelocity);      //колёса со стороны сигналящего датчика активнее
+      analogWrite(SpeedOfEngine[(i+1)%2],VOfSlow);
+      flag = 1;            //нужно ли ускорять второй мотор
     }
   }
   
   if (flag == 1 && VOfSlow < MaxVelocity){            //ускорение ворого мотора
-    VOfSlow++;                                 //it has to be in a cicle, doesn't it?     //Нет. Скорость уведичивается тогда, когда датчики спокойны. А циклично засчёт loop'а
-  } else {      
+    VOfSlow++;
+  } else {
     flag==0;
   }
   analogWrite(SpeedOfEngine[Slow],VOfSlow); 
